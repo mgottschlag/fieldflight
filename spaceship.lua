94ad0abc31be9138695b5f41119c508e8806fbf7
Mathias Gottschlag <mgottschlag@gmail.com>
2011-06-25 02:50:34 +0200

    Looooooot of bugs killed...

--- spaceship.lua
+++ spaceship.lua
@@ -3,6 +3,19 @@ require "anal/AnAL"
 vector = require "hump.vector"
 
 Spaceship = Class(function(self)
+	local v = vector(1,1)
+	local rotation = 0
+	local polarisation = 0
+	local speed = 0
+	local x = 0
+	local y = 0
+	local startX = 0
+	local startY = 0
+	local initialized --TODO weg!
+
+	--self.image
+	--self.player
+	--self.spaceship_animation
 end)
 
 -- constant
@@ -10,99 +23,104 @@ local width = 200
 local height = 200
 local scale = 0.5
 local acceleration = 0.5
--- variable
-local v = vector(1,1)
-local rotation = 0
-local polarisation = 0
-local speed = 0
-local x = 0
-local y = 0
-
-local image
-local player
-local spaceship_animation
 local spaceship_polygon = {	50, 1,											--top point
 				19, 13,  4, 37,  1, 54,  2, 91,  9, 110,  30, 133,  14, 145,  6, 162, 9, 281, 		--left side 
 				89, 281,  92, 162,  85, 145,  68, 133,  89, 110,  97, 91,  98, 54,  95, 37,  80, 13} 	--right side}
 
-function countdown( player, img_path, x, y )
-	self:backToTheRoots()
+function Spaceship:countdown( player, img_path, x, y )
 --TODO startrichtung muss von level abhängig sein
+	self.startX = x
+	self.startY = y
 	self.image = love.graphics.newImage(img_path)
+	self.spaceship_animation = newAnimation(self.image, 200, 200, 0, 1)
+	self:backToTheRoots()
 	self.player = player
-	self.x = x
-	self.y = y	
-	self.spaceship_animation = newAnimation(image, 200, 200, 0, 2)
+	self.width = 200
+	self.height = 200
+	self.scale = 0.5
+	self.acceleration = 0.5
 	-- newAnimation(plane_img, 200, 200, 0, 2)
 end
 
-function backToTheRoots()
-	local v = vector(1,1)
-	local rotation = 0
-	local speed = 0
-	local x = 0
-	local y = 0
+function Spaceship:backToTheRoots()
+	self.v = vector(1,1)
+	self.rotation = 0
+	self.speed = 0
+	self.x = self.startX
+	self.y = self.startY
 	self:invertPolarisation(1)
 end
 
-function update()
+function Spaceship:update()
 	self:draw()
+	self.initialized = 1
 end
 
-function draw()
-	updateVector()
-	calculatePosition()
--- love.graphics.draw( drawable, x, y, orientation, scaleX, scaleY, originX, originY )
-	self.spaceship_animation:draw(self.spaceship_animation,
-		self.x, self.y,
-		0, self.scale, self.scale, 
-		-(self.width / 4), -(self.height / 4))
+function Spaceship:draw()
+	self:updateVector()
+	self:calculatePosition()
+	-- love.graphics.draw( drawable, x, y, orientation, scaleX, scaleY, originX, originY )
+	self.spaceship_animation:draw(self.x, self.y,
+	0, 1, 1,
+		--0, self.scale, self.scale, 
+		-50, -50)
+	--	-(self.width / 4), -(self.height / 4))
 end
 
-function calculatePosition()
-	self.x = self.x + v.x
-	self.y = self.y + v.y
+function Spaceship:calculatePosition()
+	--TODO was geht hier nicht?
+	local add = 0
+	--if self.v ~≃ nil then 
+	--	add = self.v.x 
+	--end
+	self.x = self.x + add
+	--if (self.v =≃ nil) then else add = self.v.x end
+	self.y = self.y + add
 end
 
-function accelerate(dt)
+function Spaceship:accelerate(dt)
 	-- formel: v = a * t
 	-- TODO level.getFieldVector
 	-- spaceship_animation:move(speedX * dt, speedY * dt)
 	self.speed = self.speed + (self.acceleration * dt)
 end
 
-function rotate(degree)
+function Spaceship:rotate(degree)
 	rotation = math.mod(rotation + degre/360) --TODO testen! Doku fehlt
 	love.graphics.push(self.spaceship_animation)
 	love.graphics.rotate(rotation)
 	love.graphics.pop()
 end
 
-function updateVector()
+function Spaceship:updateVector()
 	self.v:rotate_inplace((self.rotation/360) * math.pi)
 	self.v:normalize_inplace()
 	self.v = self.v * self.speed
 end
 
-function getSpeed()
+function Spaceship:getSpeed()
 	return self.speed
 end
 
-function getPolarisation()
+function Spaceship:getPolarisation()
 	return self.polarisation
 end
 
-function invertPolarisation(polarisation)
+function Spaceship:invertPolarisation(polarisation)
 	self.polarisation = math.mod(polarisation + 1, 2) -- (polarisation +1)%2
 	self.spaceship_animation:seek(self.polarisation + 1)
 end
 
-function scaleSpaceshipPolygon()
+function Spaceship:scaleSpaceshipPolygon()
 	for x in self.spaceship_polygon do
 		x = x*self.scale
 	end
 end
 
-function rotateSpaceshipPolygon()
-  
+function Spaceship:rotateSpaceshipPolygon()
+	rot_in_deg = math.rad(self.rotation)
+	for i=1, # self.spaceship_polygon, 2 do
+  		self.spaceship_polygon[i] 	= (math.cos(rot_in_deg)*self.spaceship_polygon[i]) + (-1*math.sin(rot_in_deg)*self.spaceship_polygon[i+1])
+  		self.spaceship_polygon[i+1]	= (math.sin(rot_in_deg)*self.spaceship_polygon[i]) + (   math.cos(rot_in_deg)*self.spaceship_polygon[i+1])
+ 	end
 end
